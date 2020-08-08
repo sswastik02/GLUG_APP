746ff58e09d59b8f6423505d679d5eba3fbca85a
knightvertrag <52249024+knightvertrag@users.noreply.github.com>
2020-08-09 01:35:44 +0530

    completed notice model

--- lib/models/notice_model.dart
+++ lib/models/notice_model.dart
@@ -1,63 +1,56 @@
 import 'dart:convert';
 
-class NoticeProperties {
+class Notice {
+  Notice({
+    this.notices,
+  });
+
+  final Notices notices;
+
+  factory Notice.fromJson(Map<String, dynamic> json) => Notice(
+        notices: Notices.fromJson(json["notices"]),
+      );
+}
+
+class Notices {
+  Notices({
+    this.general,
+    this.student,
+    this.academic,
+    this.hostel,
+  });
+
+  final List<Academic> general;
+  final List<Academic> student;
+  final List<Academic> academic;
+  final List<Academic> hostel;
+
+  factory Notices.fromJson(Map<String, dynamic> json) => Notices(
+        general: List<Academic>.from(
+            json["General"].map((x) => Academic.fromJson(x))),
+        student: List<Academic>.from(
+            json["Student"].map((x) => Academic.fromJson(x))),
+        academic: List<Academic>.from(
+            json["Academic"].map((x) => Academic.fromJson(x))),
+        hostel: List<Academic>.from(
+            json["Hostel"].map((x) => Academic.fromJson(x))),
+      );
+}
+
+class Academic {
+  Academic({
+    this.title,
+    this.file,
+    this.date,
+  });
+
   final String title;
   final String file;
   final String date;
 
-  NoticeProperties({this.title, this.file, this.date});
-
-  factory NoticeProperties.fromJson(Map<String, dynamic> jsonMap) {
-    return NoticeProperties(
-      title: jsonMap['title'],
-      file: jsonMap['file'],
-      date: jsonMap['date'],
-    );
-  }
-}
-
-class NoticeCategories {
-  final List<NoticeProperties> general;
-  final List<NoticeProperties> student;
-  final List<NoticeProperties> academic;
-  final List<NoticeProperties> hostel;
-
-  NoticeCategories({this.general, this.student, this.academic, this.hostel});
-
-  factory NoticeCategories.fromJson(Map<String, dynamic> jsonMap) {
-    var generalfromjson = jsonMap['General'];
-    List<NoticeProperties> generalList =
-        new List<NoticeProperties>.from(generalfromjson);
-    var studentfromjson = jsonMap['Student'];
-    List<NoticeProperties> studentList =
-        new List<NoticeProperties>.from(studentfromjson);
-    var academicfromjson = jsonMap['Academic'];
-    List<NoticeProperties> academicList =
-        new List<NoticeProperties>.from(academicfromjson);
-    var hostelfromjson = jsonMap['Hostel'];
-    List<NoticeProperties> hostelList =
-        new List<NoticeProperties>.from(hostelfromjson);
-    return NoticeCategories(
-      general: generalList,
-      student: studentList,
-      academic: academicList,
-      hostel: hostelList,
-    );
-  }
-}
-
-class Notice {
-  final NoticeCategories notices;
-
-  Notice({this.notices});
-
-  factory Notice.fromJson(Map<String, dynamic> jsonMap) {
-    return Notice(
-      notices: NoticeCategories.fromJson(jsonMap['notices']),
-    );
-  }
-
-  // factory Notice.withError(String errorVal) {
-  //   return
-  // }
+  factory Academic.fromJson(Map<String, dynamic> json) => Academic(
+        title: json["title"],
+        file: json["file"],
+        date: json["date"],
+      );
 }
