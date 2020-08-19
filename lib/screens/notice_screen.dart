48632fc21d707840ac498aba50e7ecc62bd27987
knightvertrag <52249024+knightvertrag@users.noreply.github.com>
2020-08-20 03:00:09 +0530

    Basic Notice list view

--- lib/screens/notice_screen.dart
+++ lib/screens/notice_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:glug_app/models/notice_model.dart';
 import 'package:glug_app/blocs/notices_bloc.dart';
+import 'package:glug_app/widgets/notice_tile.dart';
 
 class NoticeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,13 @@ class NoticeScreen extends StatefulWidget {
 }
 
 class _NoticeScreenState extends State<NoticeScreen> {
+  String _dropdownvalue;
+  void changeNoticeType(String noticeType) {
+    setState(() {
+      _dropdownvalue = noticeType;
+    });
+  }
+
   @override
   void initState() {
     noticeBloc.fetchAllData();
@@ -22,30 +30,54 @@ class _NoticeScreenState extends State<NoticeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: StreamBuilder(
-        stream: noticeBloc.allNoticeData,
-        builder: (context, AsyncSnapshot<Notice> snapshot) {
-          if (snapshot.hasData) {
-            Notice notice = snapshot.data;
-            List<Academic> generalNotices = notice.notices.general;
-            return Column(
-              children: [
-                Column(
-                  children: [
-                    Text(generalNotices[1].title),
-                    Text(generalNotices[1].file),
-                    Text(
-                      generalNotices[1].date,
-                    )
-                  ],
-                )
-              ],
-            );
-          } else
-            return Center(child: CircularProgressIndicator());
-        },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notices'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder(
+              stream: noticeBloc.allNoticeData,
+              builder: (context, AsyncSnapshot<Notice> snapshot) {
+                if (snapshot.hasData) {
+                  Notice notice = snapshot.data;
+                  return ListView.builder(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 0,
+                      horizontal: 20,
+                    ),
+                    itemCount: notice.notices.academic.length,
+                    itemBuilder: (context, index) {
+                      return NoticeTile(notice: notice.notices.academic[index]);
+                    },
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+//return Container(
+//   child: StreamBuilder(
+//     stream: noticeBloc.allNoticeData,
+//     builder: (context, AsyncSnapshot<Notice> snapshot) {
+//       if (snapshot.hasData) {
+//         Notice notice = snapshot.data;
+//         List<Academic> generalNotices = notice.notices.general;
+//         return Scaffold(
+//           appBar: AppBar(
+
+//           ),
+//         );
+//       } else
+//         return Center(child: CircularProgressIndicator());
+//     },
+//   ),
+// );
